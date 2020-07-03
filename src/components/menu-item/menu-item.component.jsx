19969f9bdb281bd932f7e21c3b5fa6527338a1b8
Nazir <n_ahmad20@live.co.uk>
2020-07-03 11:52:01 +0100

    use react routing for home page. use HOC for menu item.

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import {withRouter} from "react-router-dom";
 
 import "./menu-item.styles.scss";
 
 // handles each of the home page categories
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`menu-item ${size}`}>
+const MenuItem = ({ title, imageUrl, size, history, match, linkUrl }) => (
+  <div className={`menu-item ${size}`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
     <div
       style={{ backgroundImage: `url(${imageUrl})` }}
       className="background-image"
@@ -16,4 +17,6 @@ const MenuItem = ({ title, imageUrl, size }) => (
   </div>
 );
 
-export default MenuItem;
+// HOC used to give the router props access to menu item
+// so it passes props such as history, match and locaiton
+export default withRouter(MenuItem);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+
+import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/*gives the App comp all the functionality for routing*/}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );

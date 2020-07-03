e35c7f08cbba96e7710ab95c35ec09c8f4fb02a4
Nazir <n_ahmad20@live.co.uk>
2020-07-03 18:46:35 +0100

    add header with links to home, contact and shop page.

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/Shop/shop.component";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div className="App">
+      {/*placing the header outside the switch and routers will allow it to display in every page*/}
+      <Header/>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />

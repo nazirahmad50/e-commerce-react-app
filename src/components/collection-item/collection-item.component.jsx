6e640726503de82cebb44c0f1516dd42720c44f3
Nazir <n_ahmad20@live.co.uk>
2020-07-03 18:14:53 +0100

    add shop page. display the collections in shop page with styles.

---
+++ src/components/collection-item/collection-item.component.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./collection-item.component.styles.scss";
+
+// handle each of the collection ite in shop page
+const CollectionItem = ({ id, name, price, imageUrl }) => (
+  <div className="collection-item">
+    <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
+    <div className="collection-footer">
+      <span className="name">{name}</span>
+      <span className="price">{price}</span>
+    </div>
+  </div>
+);
+
+export default CollectionItem; 
---
+++ src/components/collection-preview/collection-preview.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import "./collection-preview.styles.scss";
+
+import CollectionItem from "../collection-item/collection-item.component";
+
+// holds all the collection items as per the shop data
+const CollectionPreview = ({ title, items }) => (
+  <div className="collection-preview">
+    <h1 className="title">{title.toUpperCase()}</h1>
+    <div className="preview">
+      {items
+        .filter((item, idx) => idx < 4) 
+        .map(({id, ...itemProps}) => (
+          <CollectionItem key={id} {...itemProps}/>
+        ))}
+    </div>
+  </div>
+);
+
+export default CollectionPreview;

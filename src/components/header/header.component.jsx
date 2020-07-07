6656369511483a86b1848c03ef132f6202285856
Nazir <n_ahmad20@live.co.uk>
2020-07-07 19:03:31 +0100

    add cart icon and cart drop down. add cart redux for showing or hidding cart drop down.

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,41 +1,50 @@
 import React from "react";
-import {Link} from "react-router-dom";
-import {connect} from "react-redux";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
-import {auth} from "../../firebase/firebase-util";
+import { auth } from "../../firebase/firebase-util";
+import CartIcon from "../cart-icon/cat-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
 // special syntax in react for importing svg
-import {ReactComponent as Logo} from "../../assets/4.3 crown.svg";
+import { ReactComponent as Logo } from "../../assets/4.3 crown.svg";
 
 import "./header.component.styles.scss";
 
-const Header = ({currentUser}) =>(
-    <div className="header">
-        <Link className="logo-container" to="/">
-            <Logo className="logo"/>
-        </Link>
-        <div className="options">
-            <Link className="option" to="/shop">
-                SHOP
-            </Link>
-            <Link className="option" to="/contact">
-                Contact
-            </Link>
-                {
-                    currentUser?
-                    <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
-                    :
-                    <Link className="option" to="/signin">SIGN IN</Link>
-                }
+const Header = ({ currentUser, hidden }) => (
+  <div className="header">
+    <Link className="logo-container" to="/">
+      <Logo className="logo" />
+    </Link>
+    <div className="options">
+      <Link className="option" to="/shop">
+        SHOP
+      </Link>
+      <Link className="option" to="/contact">
+        Contact
+      </Link>
+      {currentUser ? (
+        <div className="option" onClick={() => auth.signOut()}>
+          SIGN OUT
         </div>
+      ) : (
+        <Link className="option" to="/signin">
+          SIGN IN
+        </Link>
+      )}
+      <CartIcon />
     </div>
-)
+    {hidden ? null : <CartDropdown />}
+  </div>
+);
 
 // state is the root reducer
 // gets any state we want from the redux store
-const mapStateToProps = state =>({
-    currentUser: state.user.currentUser
-})
+// advanced destructuring in the param
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
+});
 
 // 'connect' is HOC allows us modify components to have access to things related to redux
 // first args returns the state as props from redux store and passes it to the header component as props
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,11 @@
 import {combineReducers} from "redux";
 
 import userReducer from "./user/user.reducer";
+import cartReducer from "./cart/cart.reducer";
+
 
 // will combine all the reducers into one object
 export default combineReducers({
-    user:userReducer
+    user:userReducer,
+    cart:cartReducer
 })

c67b5b14a849668f80cde101287e73193884dab1
Nazir <n_ahmad20@live.co.uk>
2020-07-09 11:49:03 +0100

    remove or incerement checkout items based on arrows for quantity in checkout page.

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -16,3 +16,24 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   // add a base quantity of 1 for the cart item to add if the existingItem returns false
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
+
+// remove item from cart based on the quantity arrows
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToRemove.id
+  );
+
+  // if the existingItem quantity is at one then remove it
+  if (existingItem.quantity === 1) {
+    return cartItems.filter((item) => item.id !== cartItemToRemove.id);
+  }
+
+  // if cartItem quantity is more than one
+  // decrease the quantity of te cart item if teh cart item matches the cart to remove id
+  // otherwise return cartItem as they dont need to modified
+  return cartItems.map((cartItem) =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+   );
+};

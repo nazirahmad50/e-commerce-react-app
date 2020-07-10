f5e41154198ca347347fb3b418397d591703c4f1
Nazir <n_ahmad20@live.co.uk>
2020-07-10 15:51:25 +0100

    add collection item to colelction page and style it.

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,22 +1,31 @@
 import React from "react";
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 
 import CollectionItem from "../../components/collection-item/collection-item.component";
 
-import {selectCollection} from "../../redux/shop/shop.selectors";
+import { selectCollection } from "../../redux/shop/shop.selectors";
 
 import "./collection.styles.scss";
 
-const CollectionPage = ({ collection }) => (
-  <div className="collection-page">
-    <h2>{collection.title}</h2>
-  </div>
-);
+const CollectionPage = ({ collection }) => {
+  const { title, items } = collection;
+
+  return (
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item}/>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 // ownProps are the props of the component that is sorrounded by connect
 // pass state after invoking 'selectCollection' func
-const mapStateToProps = (state, ownProps) =>({
-  collection: selectCollection(ownProps.match.params.collectionId)(state)
-})
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state),
+});
 
 export default connect(mapStateToProps)(CollectionPage);

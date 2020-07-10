fa6c3ca7252436c04057b4df9c22d8add119f2eb
Nazir <n_ahmad20@live.co.uk>
2020-07-10 19:19:49 +0100

    use redux middlewares only in developnment mode.

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,7 +6,12 @@ import rootReducer from "./root-reducer";
 
 // store all the middlewares in an array
 // so if we need to add any extra middlewars we can add it here and will be able to spread them as args in 'applyMiddleware'
-const middlewares = [logger];
+const middlewares = [];
+
+// if in developnment mode then show middlewares
+if (process.env.NODE_ENV === "development"){
+    middlewares.push(logger);
+}
 
 // the middleware will capture any action that gets fired and we can catch them and will be able to display them
 // the middleware is in the middle between the action and the root reducer

786201e6cc93b969e0545dbcea06485627aa8920
Nazir <n_ahmad20@live.co.uk>
2020-07-10 16:04:35 +0100

    change shop data to objects of collection instead of array of objects.

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,14 +1,5 @@
 import { createSelector } from "reselect";
 
-// used to get collections based on their id
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5,
-};
-
 const selectShop = (state) => state.shop;
 
 // get the whole collections object
@@ -17,11 +8,10 @@ export const selectCollections = createSelector(
   (shop) => shop.collections
 );
 
-// get collections from redux by the the 'collectionUrlParam'
-// e.g. collection id of 1 will match with the hats url as it is an id of 1 in the collection id map
+// get collections object from redux based on the 'collectionUrlParam'
+// e.g. 
 export const selectCollection = (collectionUrlParam) =>
-  createSelector([selectCollections], (collections) =>
-    collections.find(
-      (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-    )
+  createSelector(
+    [selectCollections],
+    (collections) => collections[collectionUrlParam]
   );
